9d75e025f5a13abd34d19dc1442b59b379a8d17e
Titus-Andrei Moldovan <titus.moldovan@8x8.com>
2020-11-11 17:56:43 +0200

    adds SpotActivity

---
+++ android/app/src/main/java/org/jitsi/meet/SpotActivity.kt
@@ -0,0 +1,12 @@
+package org.jitsi.meet
+
+import android.app.Activity
+import android.os.Bundle
+
+class SpotActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot)
+    }
+}
